3252ffbb774b66de449170b14c21e9811ec13aaf
DanteOnline <iamdanteonline@gmail.com>
2025-12-26 19:20:24 +0300

    update logging

--- main.py
+++ main.py
@@ -6,14 +6,24 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from app.config import BOT_TOKEN
+from app.middlewares.logging import LoggingMiddleware
 from app.handlers import (
     common,
     about,
     faq,
     form,
+    errors,
 )
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format=(
+        "%(asctime)s | %(levelname)s | "
+        "%(name)s | %(message)s"
+    ),
+)
+
+logger = logging.getLogger("bot")
 
 dp = Dispatcher()
 
@@ -21,10 +31,21 @@ async def main() -> None:
     """
     Запуск бота
     """
+
+    # middlewares
+    dp.message.middleware(LoggingMiddleware())
+    dp.callback_query.middleware(LoggingMiddleware())
+
+    # routers
+    # для ошибок
+    dp.include_router(errors.router)
+    # основные
     dp.include_router(common.router)
     dp.include_router(about.router)
     dp.include_router(faq.router)
     dp.include_router(form.router)
+
+    # запуск бота
     bot = Bot(token=BOT_TOKEN)
     await dp.start_polling(bot)
 
